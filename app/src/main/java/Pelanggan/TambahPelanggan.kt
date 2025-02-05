025abb7a393ad24780f0188df114f70694629f4f
VionaRhema17 <viona.rhema07@gmail.com>
2025-02-05 12:54:48 +0700

    penambahan logika

--- app/src/main/java/Pelanggan/TambahPelanggan.kt
+++ app/src/main/java/Pelanggan/TambahPelanggan.kt
@@ -11,7 +11,7 @@ class tambahPelanggan : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tambah_pelanggan)
+        setContentView(R.layout.activity_laman_tambah_pelanggan)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
